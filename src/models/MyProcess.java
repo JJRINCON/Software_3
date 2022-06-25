a75d0327e425ca672c753b6360c048722b66555e
JhonatanD09 <joranada@gmail.com>
2022-06-24 21:23:33 -0500

    backend 1.0

---
+++ src/models/MyProcess.java
@@ -0,0 +1,62 @@
+package models;
+
+public class MyProcess {
+
+	private String name;
+	private double time;
+	private boolean locked, isSuspended_Locked, isSuspended_Ready;
+
+	public MyProcess(String name, double time, boolean... states) {
+		super();
+		this.name = name;
+		this.time = time;
+		this.locked = states[0];
+		this.isSuspended_Locked = states[1];
+		this.isSuspended_Ready = states[2];
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = (this.time - time);
+	}
+
+	public void updateTime(int time) {
+		this.time = time;
+	}
+
+	public boolean isLocked() {
+		return locked;
+	}
+
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+	
+	public boolean isSuspended_Locked() {
+		return isSuspended_Locked;
+	}
+	
+	public boolean isSuspended_Ready() {
+		return isSuspended_Ready;
+	}
+	
+	public void setSuspended_Locked(boolean isSuspended_Locked) {
+		this.isSuspended_Locked = isSuspended_Locked;
+	}
+	
+	public void setSuspended_Ready(boolean isSuspended_Ready) {
+		this.isSuspended_Ready = isSuspended_Ready;
+	}
+
+}
---
+++ src/models/OperatingSystem.java
@@ -0,0 +1,399 @@
+package models;
+
+import java.util.ArrayList;
+
+
+public class OperatingSystem {
+
+	private Queue<MyProcess> processQueueReady;
+	private ArrayList<MyProcess> readyAndDespachado;
+	private ArrayList<MyProcess> executing;
+	private ArrayList<MyProcess> expired;
+	private ArrayList<MyProcess> processTerminated;
+	private ArrayList<MyProcess> locked;
+	private ArrayList<MyProcess> toLocked;
+	private ArrayList<MyProcess> lockedToReady;
+	private ArrayList<MyProcess> lockedToSuspendedLocked;
+	private ArrayList<MyProcess> suspendedLocked;
+	private ArrayList<MyProcess> suspendedLockedToLocked;
+	private ArrayList<MyProcess> suspendedLockedToSuspendedReady;
+	private ArrayList<MyProcess> toSuspendedReady;
+	private ArrayList<MyProcess> suspendedReady;
+	private ArrayList<MyProcess> suspendedReadyToReady;
+	
+
+	public OperatingSystem() {
+		this.readyAndDespachado = new ArrayList<>();
+		this.executing = new ArrayList<>();
+		this.expired = new ArrayList<>();
+		this.processTerminated = new ArrayList<>();
+		this.locked =new ArrayList<>();
+		this.toLocked =new ArrayList<>();
+		this.lockedToReady = new ArrayList<>();
+		this.lockedToSuspendedLocked =new ArrayList<>();
+		this.suspendedLocked =new ArrayList<>();
+		this.suspendedLockedToLocked =new ArrayList<>();
+		this.suspendedLockedToSuspendedReady = new ArrayList<>();
+		this.toSuspendedReady=new ArrayList<>();
+		this.suspendedReady =new ArrayList<>();
+		this.suspendedReadyToReady =new ArrayList<>();
+	}
+
+	public boolean addProcess(MyProcess myProcess) {
+		if (search(myProcess.getName()) == null) {
+			readyAndDespachado.add(new MyProcess(myProcess.getName(), myProcess.getTime(), myProcess.isLocked()));
+			addProcess( readyAndDespachado,myProcess, false);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Me avisa si no funciona, xd
+	 * @param actualName
+	 * @param name
+	 * @param time
+	 * @param lockedStatus
+	 */
+	public void editProcess(String actualName, String name, int time, boolean... states) {
+		edit(search(actualName), name, time, states);
+		edit(searchInList(actualName, readyAndDespachado), name, time, states);
+	}
+	
+	private void edit(MyProcess myProcess, String name, int time, boolean ... states) {
+		myProcess.setName(name);
+		myProcess.updateTime(time);
+		myProcess.setLocked(states[0]);
+		myProcess.setSuspended_Locked(states[1]);
+		myProcess.setSuspended_Ready(states[2]);
+	}
+	
+	/**
+	 * Eliminar de la cola y de la lista de listos
+	 * @param name
+	 * @return
+	 */
+	public boolean deleteProccess(String name) {
+		boolean isDelete = false;
+		Node<MyProcess> temp = processQueueReady.peek();
+		readyAndDespachado.remove(searchInList(name, readyAndDespachado));
+		if (temp.getData().getName().equals(name)) {
+			processQueueReady.pop();
+			isDelete = true;
+		}else {
+			isDelete = deleteProcess(name, isDelete, temp);
+		}	
+		return isDelete;
+	}
+
+	private boolean deleteProcess(String name, boolean isDelete, Node<MyProcess> temp) {
+		while (temp.getNext() != null) {
+			if (temp.getNext().getData().getName().equals(name)) {
+				temp.setNext(temp.getNext().getNext());
+				isDelete = true;
+			} else {
+				temp = temp.getNext();
+			}
+		}
+		return isDelete;
+	}
+
+	private MyProcess searchInList(String name, ArrayList<MyProcess> myProcesses) {
+		for (MyProcess myProcess : myProcesses) {
+			if (name.equals(myProcess.getName())) {
+				return myProcess;
+			}
+		}
+		return null;
+	}
+	
+	
+	public MyProcess search(String name) {
+		Node<MyProcess> temp = processQueueReady.peek();
+		while (temp != null) {
+			if (temp.getData().getName().equals(name)) {
+				return temp.getData();
+			} else {
+				temp = temp.getNext();
+			}
+		}
+		return null;
+	}
+
+	public void startSimulation() {
+		while (!processQueueReady.isEmpty()) {
+			MyProcess process = processQueueReady.peek().getData();
+			valideSystemTimer(process);
+		}
+	}
+
+	private void valideSystemTimer(MyProcess process) {
+		addProcess(executing, process, true);
+		if ((process.getTime() - 5) > 0) {
+			proccessTimeDiscount(process);
+		} else {
+			MyProcess myProcess = processQueueReady.pop();
+			myProcess.setTime((int)myProcess.getTime());
+			addProcess(processTerminated, myProcess, false);
+		}
+	}
+
+	private void proccessTimeDiscount(MyProcess process) {
+		process.setTime(5);
+		valideLocked(process);
+//		readyAndDespachado.add(new MyProcess(process.getName(), process.getTime(), process.isLocked()));
+//		processQueueReady.push(processQueueReady.pop());
+	}
+
+	private void valideLocked(MyProcess process) {
+		if (process.isLocked()) {
+			addProcess(toLocked, process, false);
+			addProcess(locked,process,false);
+			if (process.isSuspended_Locked()) {
+				addProcess(lockedToSuspendedLocked, process, false);
+				addProcess(suspendedLocked, process, false);
+				if(process.isSuspended_Ready()) {
+					addProcess(suspendedLockedToSuspendedReady, process, false);
+					addProcess(suspendedReady, process, false);
+					addProcess(suspendedReadyToReady, process, false);
+				}else {
+					addProcess(suspendedLockedToLocked, process, false);
+					addProcess(locked, process, false);
+					addProcess(lockedToReady, process, false);
+				}
+			}else {
+				addProcess(lockedToReady, process, false);
+			}
+		}else if(process.isSuspended_Ready()) {
+			addProcess(toSuspendedReady, process, false);
+			addProcess(suspendedReady, process, false);
+			addProcess(suspendedReadyToReady, process, false);
+		}else {
+			addProcess(expired, process, false);
+		}
+	}
+
+	
+	private void addProcess(ArrayList<MyProcess> myProcesses, MyProcess myProcess, boolean isExecuting) {
+		boolean[] states = new boolean[] { myProcess.isLocked(), myProcess.isSuspended_Locked(), myProcess.isSuspended_Ready()};
+		if (isExecuting) {
+			myProcesses
+					.add(new MyProcess(myProcess.getName(), (myProcess.getTime() - 5 < 0 ? 0 : myProcess.getTime() - 5), states));
+		} else {
+			myProcesses.add(new MyProcess(myProcess.getName(), myProcess.getTime(), states));
+		}
+	}
+	
+	/**
+	 * 
+	 * @return Los procesos que se van agregando a la lista, estos toca ir actualizando
+	 * cada que se agregan a la interfaz
+	 */
+	public Queue<MyProcess> getProcessQueue() {
+		return processQueueReady;
+	}
+
+	public void verifyProcessName(String name) throws Exception {
+		Node<MyProcess> temp = processQueueReady.peek();
+		while(temp != null){
+			if(temp.getData().getName().equals(name)){
+				throw new Exception("Nombre de proceso no disponible");
+			}
+			temp = temp.getNext();
+		}
+	}
+
+/**
+ * 
+ * @return listos  y despachados
+ */
+	public ArrayList<MyProcess> getReadyAndDespachado() {
+		return readyAndDespachado;
+	}
+	
+	/**
+	 * 
+	 * @return en ejecucion
+	 */
+	public ArrayList<MyProcess> getExecuting() {
+		return executing;
+	}
+	
+	/**\
+	 * 
+	 * @return expirados
+	 */
+	public ArrayList<MyProcess> getExpired() {
+		return expired;
+	}
+	
+	/**
+	 * 
+	 * @return terminados
+	 */
+	public ArrayList<MyProcess> getProcessTerminated() {
+		return processTerminated;
+	}
+	
+	/**
+	 * 
+	 * @return a bloqueado
+	 */
+	public ArrayList<MyProcess> getToLocked() {
+		return toLocked;
+	}
+	
+	/**
+	 * 
+	 * @return bloqueados
+	 */
+	public ArrayList<MyProcess> getLocked() {
+		return locked;
+	}
+	
+	/**
+	 * 
+	 * @return de bloqueado a listo
+	 */
+	public ArrayList<MyProcess> getLockedToReady() {
+		return lockedToReady;
+	}
+	
+	/**
+	 * 
+	 * @return de bloqueado a suspendido listo
+	 */
+	public ArrayList<MyProcess> getLockedToSuspendedLocked() {
+		return lockedToSuspendedLocked;
+	}
+	
+	/**
+	 * 
+	 * @return suspendidos bloqueados
+	 */
+	public ArrayList<MyProcess> getSuspendedLocked() {
+		return suspendedLocked;
+	}
+	
+	/**
+	 * 
+	 * @return suspendido bloqueado a bloqueado
+	 */
+	public ArrayList<MyProcess> getSuspendedLockedToLocked() {
+		return suspendedLockedToLocked;
+	}
+	
+	/**
+	 * 
+	 * @return suspendido bloqueado a suspendido listo
+	 */
+	public ArrayList<MyProcess> getSuspendedLockedToSuspendedReady() {
+		return suspendedLockedToSuspendedReady;
+	}
+	
+	/**
+	 * 
+	 * @return de ejecucion a suspendido listo
+	 */
+	public ArrayList<MyProcess> getToSuspendedReady() {
+		return toSuspendedReady;
+	}
+	
+	/**
+	 * 
+	 * @return suspendiddo listo
+	 */
+	public ArrayList<MyProcess> getSuspendedReady() {
+		return suspendedReady;
+	}
+	
+	/**
+	 * 
+	 * @return suspendido listo a listo
+	 */
+	public ArrayList<MyProcess> getSuspendedReadyToReady() {
+		return suspendedReadyToReady;
+	}
+	
+	public void show() {
+		System.out.println("Listos y despachados");
+		for (MyProcess myProcess : readyAndDespachado) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("En ejecucion");
+		for (MyProcess myProcess : executing) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Expirados");
+		for (MyProcess myProcess : expired) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("A bloqueo");
+		for (MyProcess myProcess : toLocked) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Bloqueados");
+		for (MyProcess myProcess : locked) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("De bloqueado a listo");
+		for (MyProcess myProcess : lockedToReady) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("De bloqueado a suspendido bloqueado");
+		for (MyProcess myProcess : lockedToSuspendedLocked) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Suspendido bloqueado");
+		for (MyProcess myProcess : suspendedLocked) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Suspendido bloqueado a bloqueado");
+		for (MyProcess myProcess : suspendedLockedToLocked) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Suspendido bloqueado a suspendido listo");
+		for (MyProcess myProcess : suspendedLockedToSuspendedReady) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("A Suspendido listo");
+		for (MyProcess myProcess : toSuspendedReady) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Suspendido listo");
+		for (MyProcess myProcess : suspendedReady) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Suspendido listo a listo");
+		for (MyProcess myProcess : suspendedReadyToReady) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+		
+		System.out.println("Terminados");
+		for (MyProcess myProcess : processTerminated) {
+			System.out.println(myProcess.getName()+" "+ myProcess.getTime());
+		}
+	}
+	
+	public static Object[][] processInfo(ArrayList<MyProcess> processes){
+		Object[][] processInfo = new Object[processes.size()][3];
+		for (int i = 0; i < processes.size(); i++) {
+			processInfo[i][0] = processes.get(i).getName();
+			processInfo[i][1] = processes.get(i).getTime();
+			processInfo[i][2] = processes.get(i).isLocked();
+		}
+		return processInfo;
+	}
+}
